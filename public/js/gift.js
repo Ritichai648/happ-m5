ae81249b87faf1623c3a3cc84c6b693a79b180b5
Ritichai648 <tonkabitcion@gmail.com>
2024-06-19 20:01:55 +0700

    first commit

---
+++ public/js/gift.js
@@ -0,0 +1,11 @@
+function SendGift() {
+    Swal.fire({
+        title: "เก็บมันไว้ดีๆนะค้าบ",
+        icon: "success",
+        text: "Iwillloveyouforever@gmail.com",
+        color: "#fdfdfd",
+        background: "linear-gradient(#ffaad9,#fffdfe)",
+        confirmButtonColor: '#f5788a', // optional for button styling
+        confirmButtonText: 'รับของขวัญ',
+      });
+}
